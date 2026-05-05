184a3b51fc0313ea05e96a2338ba6ea4f32efd55
ulilabzr <absoruu@gmail.com>
2026-05-05 22:18:18 +0700

    Add Inter Variable font styles with multiple unicode ranges

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes ;
 
     /**
      * The attributes that are mass assignable.
@@ -54,5 +55,17 @@ class User extends Authenticatable
     public function productCategories()
     {
         return $this->hasMany(ProductCategory::class);
+    }
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+    public function subscription()
+    {
+        return $this->hasOne(Subscription::class);
     }
 }
